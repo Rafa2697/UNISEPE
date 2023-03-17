39b425c57e3589f0476e8b26ab422bbf09bda7d0
Rafael <Rafael.mattos468@gmail.com>
2023-03-17 20:05:30 -0300

    update

--- programação estruturada/unidade 2/listaCirculares.c
+++ programação estruturada/unidade 2/listaCirculares.c
@@ -1,41 +1,69 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
 
-struct item
-{
+#include <stdlib.h>
+
+ 
+
+struct item{
+
     int dado;
+
     struct item *proximo;
+
 };
 
-
+ 
 
 int main(){
+
     struct item *listaCircular = NULL;
+
     struct item *itemTemp;
+
     int indice = 0;
-    //leitura dos dados
+
+    // Leitura dos dados
 
     do{
-        itemTemp = (struct item*)calloc(sizeof(struct item), 1);
-        itemTemp -> dado = indice + 1;
-        if(listaCircular == NULL){
-            itemTemp -> proximo = itemTemp;
-        }else{
-            itemTemp -> proximo = listaCircular -> proximo;
-            listaCircular -> proximo = itemTemp;
-        }
-        listaCircular = itemTemp;
-        indice++;
-    }while (indice < 3);
-   //impressão dos dados
-   itemTemp = listaCircular;
-   do
-   {
-    printf("%i\t", itemTemp -> dado);
-    itemTemp = itemTemp -> proximo;
-   } while (itemTemp != listaCircular);
 
-   return 0;
-   
-    
+        itemTemp = (struct item*) calloc(sizeof(struct item), 1);
+
+        itemTemp->dado = indice + 1;
+
+        if(listaCircular == NULL){
+
+            itemTemp->proximo = itemTemp;
+
+        }else{
+
+            itemTemp->proximo = listaCircular->proximo;
+
+            listaCircular->proximo = itemTemp;
+
+        }
+
+        listaCircular = itemTemp;
+
+        indice++;
+
+    }while(indice < 4);
+
+ 
+
+    // Impressão dos dados
+
+    itemTemp = listaCircular;
+
+    do{
+
+        printf("%i\t", itemTemp->dado);
+
+        itemTemp = itemTemp->proximo;
+
+    }while(itemTemp !=  listaCircular);
+
+ 
+
+    return 0;
+
 }
