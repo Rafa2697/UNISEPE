d4206404f8b794443d79f103d32080d08de5f25f
Rafael <93839545+Rafa2697@users.noreply.github.com>
2023-04-02 17:31:42 -0300

    Create exercicio.c

---
+++ programação estruturada/arvores/binarias/exercicio.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//estrutura nó
+typedef struct no{
+    int valor;
+    struct  no *direita, *esquerda;
+    
+}NoArv;
+
+NoArv* inserir_versao_1(NoArv *raiz, int num){
+    if (raiz == NULL){
+        NoArv *aux = malloc(sizeof(NoArv));
+        aux->valor = num;
+        aux->esquerda = NULL;
+        aux->direita = NULL;
+
+        return aux;
+    }else{
+        if (num < raiz->valor){
+            raiz->esquerda = inserir_versao_1(raiz->esquerda, num); 
+        }else{
+            raiz->direita = insrir_versao_1(raiz->direita, num);
+
+            return raiz;
+
+        }
+        
+    }
+    
+    
+}
+
+int main(){
+    NoArv *raiz = NULL;
+    raiz = inserir_versao_1(raiz, 50);
+    return 0;
+}
