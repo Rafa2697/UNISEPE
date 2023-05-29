d11d3de46b9278f82948fb4c2e8a812a24aa6efc
Rafael <Rafael.mattos468@gmail.com>
2023-05-29 12:13:58 -0300

    update

---
+++ algoritimos e lógica/exemplo live/live12.c
@@ -0,0 +1,16 @@
+#include <std
+
+//Tipo de registro - Estrutura Heterogênea
+struct Aluno{
+int matricula;
+float nota;
+int idade;
+}
+
+//um vetor é uma - Estrutura Homogênea Estrutura homogêea
+int vetor[5];
+vetor[0] = 10;
+vetor[1] = 20;
+vetor[2] = 30;
+vetor[3] = 40;
+vetor[4] = 40;
