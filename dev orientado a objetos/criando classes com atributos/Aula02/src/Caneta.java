55fe2a6632e2f2c15937cf6e3fda0162492f36bb
Rafael <Rafael.mattos468@gmail.com>
2023-08-14 17:53:30 -0300

    update

---
+++ dev orientado a objetos/criando classes com atributos/Aula02/src/Caneta.java
@@ -0,0 +1,25 @@
+public class Caneta {
+     
+        String modelo;
+        String cor;
+        float ponta;
+        int carga;
+        boolean tampada;
+
+        void status(){
+            System.out.println("Uma caneta " + this.cor);
+            System.out.println(this.tampada);
+        }
+
+        void rabiscar(){
+
+        }
+        void tampar(){
+
+        }
+        void destampar(){
+
+        }
+        
+    
+}
---
+++ dev orientado a objetos/vetores e matrizes/declaração de vetores/src/vetores.java
@@ -0,0 +1,11 @@
+public class vetores {
+    public static void main(String[] args){
+        int v[] = new int[4];
+        v[0] = 2;
+        v[2] = 7;
+
+        for(int i = 0; i < v.length; i++){
+            System.out.println(v[i]);
+        }
+    }
+} //fim da classe
