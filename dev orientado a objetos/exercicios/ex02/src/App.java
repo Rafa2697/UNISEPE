311da664906860fbd9c364bb267d67bcb5583677
Rafael <93839545+Rafa2697@users.noreply.github.com>
2023-08-19 16:47:58 -0300

    update
    exercicio de conta do banco

--- dev orientado a objetos/exercicios/ex02/src/App.java
+++ dev orientado a objetos/exercicios/ex02/src/App.java
@@ -1,5 +1,26 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Conta cc = new Conta(1, "corrente", "rafael", 150.00f, false);
+        Conta p1 = new Conta();
+       
+
+        p1.setDono("jubileu");
+        p1.setNumConta(1111);
+        p1.abrirConta("cc");
+        
+
+        Conta p2 = new Conta();
+        p2.setDono("creuza");
+        p2.setNumConta(2222);
+        p2.abrirConta("cp");
+
+        p1.depositar(100f);
+        p2.depositar(500f);
+        p2.sacar(100f);
+
+        p1.estadoAtual();
+        p2.estadoAtual();
+
+       
+        
     }
 }
--- dev orientado a objetos/exercicios/ex02/src/Conta.java
+++ dev orientado a objetos/exercicios/ex02/src/Conta.java
@@ -1,17 +1,17 @@
 public class Conta {
+    //atributos
     public int numConta;
     protected String tipo;
     private String dono;
     private Float saldo;
     private boolean status;
 
-    public Conta(int n, String t, String d, float s, boolean st){
-        this.setNumConta(n);
-        this.setTipo(t);
-        this.setDono(d);
-        this.setSaldo(s);
-        this.setStatus(st);
+    //metodo especial (contructor) será executado sempre que uma nova classe for estanciada.(virar um objeto).
+    public Conta(){
+        this.setSaldo(0f);
+        this.setStatus(false);
     }
+    //metodos get e set
     //numconta
     public int getNumConta(){
         return this.numConta;
@@ -27,10 +27,10 @@ public class Conta {
         this.tipo = t;
     }
     //dono da conta
-    private String getDono(){
+    public String getDono(){
         return this.dono;
     }
-    private void setDono(String d){
+    public void setDono(String d){
         this.dono = d;
     }
     //saldo da conta
@@ -46,6 +46,71 @@ public class Conta {
     }
     private void setStatus(boolean st){
         this.status = st;
+    }
+    //métodos personalizados
+
+    public void abrirConta(String t){
+        this.setTipo(t);
+        this.setStatus(true);
+        if (t == "cc") {
+            this.setSaldo(50f);
+        } else if(t == "cp"){
+            this.setSaldo(150f);
+        }
+        System.out.println("conta aberta com sucesso");
+    }
+    public void fecharConta(){
+        if(this.getSaldo() > 0){
+            System.out.println("Conta não pode ser fechada pois ainda tem dinheiro.");
+        }else if(this.getSaldo() < 0){
+            System.out.println("Conta não pode ser fechada pois tem débitos.");
+        }else{
+            this.setStatus(false);
+            System.out.println("Conta fechada com sucesso!");
+        }
+    }
+    public void depositar(float v){
+        if(this.getStatus()){
+            this.setSaldo(this.getSaldo() + v);
+            System.out.println("Deposito realizado com sucesso na contra de " + this.getDono());
+        }else{
+            System.out.println("Impossivel depositar em uma conta fechada");
+        }
+    }
+    public void sacar(float v){
+        if(this.getStatus()){
+           if(this.getSaldo() >= v){
+            this.setSaldo(this.getSaldo() - v);
+            System.out.println("Saque realizado na conta de " + this.getDono());
+           }else{
+            System.out.println("Saldo insuficiente para saque");
+           }
+        }else{
+           System.out.println("Impossivel sacar de uma conta fechada!");
+        }
+    }
+    public void pegarMensal(){
+        int v = 0;
+        if(this.getTipo() == "cc"){
+            v = 12;
+        }else if(this.getTipo() == "cp"){
+            v = 20;
+        }
+        if(this.getStatus()){
+            this.setSaldo(this.getSaldo() - v);
+            System.out.println("mensaliadade paga por " + this.getDono());
+        }else{
+            System.out.println("impossivel pagar em uma conta fechada");
+        }
+    }
+
+    public void estadoAtual(){
+        System.out.println("==========================================");
+        System.out.println("dono da conta: " + this.dono);
+        System.out.println("numero da conta: " + this.numConta);
+        System.out.println("tipo de conta: " + this.getTipo());
+        System.out.println("quantia na conta: R$:" + this.getSaldo());
+        System.out.println("Status " + this.getStatus());
     }
 
 }
